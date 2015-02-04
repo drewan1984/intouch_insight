3428faf5ebda42bcf5381d370811e5362a8a24a1
drewan <drewan1984@gmail.com>
2015-02-03 23:25:47 -0500

    Adding missing service files.

---
+++ MedalService/Services/IMedalStatService.cs
@@ -0,0 +1,125 @@
+﻿using MedalService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MedalService.Services
+{
+    /// <summary>
+    /// Provides medal count information about Athletes and Countries.
+    /// </summary>
+    public interface IMedalStatService
+    {
+        /// <summary>
+        /// Get the list of supported years.
+        /// </summary>
+        /// <returns>A list of supported years.</returns>
+        List<int> GetYears();
+
+        /// <summary>
+        /// Get the list of supported sports.
+        /// </summary>
+        /// <returns>A list of supported sports</returns>
+        List<string> GetSports();
+
+        /// <summary>
+        /// Get Country medal details by Country name.
+        /// </summary>
+        /// <param name="countryName">The country name.</param>
+        /// <returns>Country medal details.</returns>
+        ICountry GetCountry(string countryName);
+
+        /// <summary>
+        /// Get Country meda details by Country name for a given year.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <param name="countryName">The country name.</param>
+        /// <returns>Country medal details.</returns>
+        ICountry GetCountry(int year, string countryName);
+
+        /// <summary>
+        /// Gets a list of country medal details with optional query parameters. (Sort, Count, Start)
+        /// </summary>
+        /// <param name="queryParams">Query parametrs for the request.</param>
+        /// <returns>List of country medal details by the specified query parameters.</returns>
+        List<ICountry> GetCountries(IQueryParams queryParams = null);
+
+        /// <summary>
+        /// Gets a list of country medal details for given year with optional query parameters. (Sort, Count, Start)
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <param name="queryParams">Query parameters for the request.</param>
+        /// <returns>List of country medals for given year by the specified query parameters.</returns>
+        List<ICountry> GetCountries(int year,IQueryParams queryParams = null);
+ 
+        /// <summary>
+        /// Gets the medal details for an athlete by their unique id.
+        /// </summary>
+        /// <param name="id">An unique athelte id provided by the service.</param>
+        /// <returns>An athletes medal details.</returns>
+        IAthlete GetAlthlete(string id);
+
+        /// <summary>
+        /// Gets the medal details for an athlete for a specified year.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <param name="id">An unique athelte id provided by the service.</param>
+        /// <returns>An athletes medal details for a specified year.</returns>
+        IAthlete GetAlthlete(int year, string id);
+
+        /// <summary>
+        /// Gets a list of athletes with optional query parameters. (Sort, Count, Start)
+        /// </summary>
+        /// <param name="queryParams">Query parameters for the request.</param>
+        /// <returns>A list of athlete medal details.</returns>
+        List<IAthlete> GetAthletes(IQueryParams queryParams = null);
+
+        /// <summary>
+        /// Gets a list of athletes by country with optional query parameters. (Sort, Count, Start)
+        /// </summary>
+        /// <param name="countryName">The athletes country.</param>
+        /// <param name="queryParams">Query parameters for the request.</param>
+        /// <returns>A list of athlete medal details by country.</returns>
+        List<IAthlete> GetAthletes(string countryName, IQueryParams queryParams = null);
+
+        /// <summary>
+        /// Gets a list of athletes by country with optional query parameters. (Sort, Count, Start)
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <param name="queryParams">Query parameters for the request.</param>
+        /// <returns>A list of athlete medal details by year.</returns>
+        List<IAthlete> GetAthletes(int year, IQueryParams queryParams = null);
+
+        /// <summary>
+        /// Gets a list of athletes by country and year with optional query parameters. (Sort, Count, Start)
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <param name="countryName">The athletes country.</param>
+        /// <param name="queryParams">Query parameters for the request.</param>
+        /// <returns>A list of athlete medal details by country and year.</returns>
+        List<IAthlete> GetAthletes(int year, string countryName,IQueryParams queryParams = null);
+
+        /// <summary>
+        /// Gets sport medal details by country.
+        /// </summary>
+        /// <param name="countryName">The country name.</param>
+        /// <returns>The sport medal details.</returns>
+        List<ISportMedal> GetSportMedals(string countryName);
+
+        /// <summary>
+        /// Gets the sport medal details by year. 
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns>The sport medal details.</returns>
+        List<ISportMedal> GetSportMedals(int year);
+
+        /// <summary>
+        /// Gets the sport medal details by year and country.
+        /// </summary>
+        /// <param name="countryName">The country name.</param>
+        /// <param name="year">The year.</param>
+        /// <returns>The sport medal details.</returns>
+        List<ISportMedal> GetSportMedals(string countryName, int year);
+    }
+}
---
+++ MedalService/Services/Impl/MedalStatService.cs
@@ -0,0 +1,273 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDB.Driver.Builders;
+using MedalService.Util;
+using MedalService.Models;
+using MedalService.Models.Impl;
+
+
+namespace MedalService.Services.Impl
+{
+    /// <summary>
+    /// IMedalStatService MongoDBImplementation.
+    /// Retrieves data from a mongo database source.
+    /// </summary>
+    public class MedalStatServiceMongoDB : IMedalStatService
+    {
+        private MongoClient client;
+        private MongoServer server;
+        private MongoDatabase database;
+        private MongoCollection<Athlete> athletes;
+
+        public MedalStatServiceMongoDB()
+        {
+            client = new MongoClient("mongodb://localhost");
+            server = client.GetServer();
+            database = server.GetDatabase("oymdb");
+            athletes = database.GetCollection<Athlete>("medal_winners");
+        }
+
+        public List<int> GetYears()
+        {
+            var result = default(List<int>);
+            var aggr = athletes.Aggregate(new AggregateArgs 
+            { 
+                Pipeline = new [] 
+                { 
+                    AthleteBsonUtil.Groups.Year,
+                    AthleteBsonUtil.Aggregates.Sort(AthleteBsonUtil.Fields.Id, Sort.Ascending)
+                } 
+            });
+            result = aggr.Select(a => (int)a.First().Value).ToList();
+            return result;
+        }
+        public List<string> GetSports()
+        {
+            var result = default(List<string>);
+            var aggr = athletes.Aggregate(new AggregateArgs
+            {
+                Pipeline = new[] { AthleteBsonUtil.Groups.Sport }
+            });
+            result = aggr.Select(a => (string)a.First().Value).ToList();
+            return result;
+        }
+
+        private List<ICountry> _getCountryGroups(IEnumerable<BsonDocument> pipeline, IQueryParams queryParams)
+        {
+            var result = default(List<ICountry>);
+
+            var groupAggregate = athletes.Aggregate(new AggregateArgs { Pipeline = pipeline });
+
+            if (queryParams != null && queryParams.Count != null)
+            {
+                if (queryParams.Start != null)
+                {
+                    result = groupAggregate
+                        .Skip(queryParams.Start.Value)
+                        .Take(queryParams.Count.Value)
+                        .Select(BsonSerializer.Deserialize<Country>)
+                        .ToList<ICountry>();
+                }
+                else
+                {
+                    result = groupAggregate
+                    .Take(queryParams.Count.Value)
+                    .Select(BsonSerializer.Deserialize<Country>)
+                    .ToList<ICountry>();
+                }
+            }
+            else
+            {
+                result = groupAggregate
+                    .Select(BsonSerializer.Deserialize<Country>)
+                    .ToList<ICountry>();
+            }
+
+            return result;
+        }
+        public List<ICountry> GetCountries(IQueryParams queryParams = null)
+        {
+            var pipeline = new List<BsonDocument>();
+            pipeline.Add(AthleteBsonUtil.Groups.Country);
+            _applyQueryParams(pipeline,queryParams);
+            return _getCountryGroups(pipeline, queryParams);
+        }
+        public List<ICountry> GetCountries(int year, IQueryParams queryParams = null)
+        {
+            var pipeline = new List<BsonDocument>();
+            pipeline.Add(AthleteBsonUtil.Aggregates.Match(AthleteBsonUtil.Fields.Year,year));
+            pipeline.Add(AthleteBsonUtil.Groups.Country);
+            _applyQueryParams(pipeline, queryParams);
+            return _getCountryGroups(pipeline, queryParams);
+        }
+        public ICountry GetCountry(string countryName)
+        {
+            var pipeline = new List<BsonDocument>();
+            pipeline.Add(AthleteBsonUtil.Aggregates.Match(AthleteBsonUtil.Fields.Country,countryName));
+            pipeline.Add(AthleteBsonUtil.Groups.Country);
+            return _getCountryGroups(pipeline,null)
+                 .FirstOrDefault<ICountry>();
+        }
+        public ICountry GetCountry(int year, string countryName)
+        {
+            var pipeline = new List<BsonDocument>();
+            pipeline.Add(AthleteBsonUtil.Aggregates.Match(AthleteBsonUtil.Fields.Year,year));
+            pipeline.Add(AthleteBsonUtil.Aggregates.Match(AthleteBsonUtil.Fields.Country,countryName));
+            pipeline.Add(AthleteBsonUtil.Groups.Country);
+            return _getCountryGroups(pipeline, null)
+                 .FirstOrDefault<ICountry>();
+        }
+
+        private List<IAthlete> _getAthletes(IEnumerable<BsonDocument> pipeline, IQueryParams queryParams)
+        {
+            var result = default(List<IAthlete>);
+            IEnumerable<BsonDocument> groupAggregate;
+
+            if (pipeline != null)
+                groupAggregate = athletes.Aggregate(new AggregateArgs { Pipeline = pipeline });
+            else
+                groupAggregate = athletes.AsQueryable<BsonDocument>();
+
+
+            if (queryParams != null && queryParams.Count != null)
+            {
+                if (queryParams.Start != null)
+                {
+                    result = groupAggregate
+                        .Skip(queryParams.Start.Value)
+                        .Take(queryParams.Count.Value)
+                        .Select(BsonSerializer.Deserialize<Athlete>)
+                        .ToList<IAthlete>();
+                }
+                else
+                {
+                    result = groupAggregate
+                    .Take(queryParams.Count.Value)
+                    .Select(BsonSerializer.Deserialize<Athlete>)
+                    .ToList<IAthlete>();
+                }
+            }
+            else
+            {
+                result = groupAggregate
+                    .Select(BsonSerializer.Deserialize<Athlete>)
+                    .ToList<IAthlete>();
+            }
+
+            return result;
+        }
+        private IAthlete _getAthlete(Func<Athlete,Boolean> condition)
+        {
+            return athletes
+            .AsQueryable<Athlete>()
+            .FirstOrDefault(condition);
+        }
+        private bool _matchAthleteYear(Athlete a, int year)
+        {
+            return (a.Year.Equals(year));
+        }
+        private bool _matchAthleteName(Athlete a, string name)
+        {
+            return (a.Name.Equals(name));
+        }
+        private bool _matchAthleteCountry(Athlete a, string country)
+        {
+            return (a.Country.Equals(country));
+        }
+        private bool _matchAthleteId(Athlete a, string id)
+        {
+            return (a.Id.Equals(id));
+        }
+
+        public IAthlete GetAlthlete(string id)
+        {
+            var result = _getAthlete((a => _matchAthleteId(a, id)));
+            return result;
+        }
+        public IAthlete GetAlthlete(int year, string id)
+        {
+            var result = _getAthlete(
+                (a =>
+                    _matchAthleteId(a, id) &&
+                    _matchAthleteYear(a, year))
+                );
+            return result;
+        }
+        public List<IAthlete> GetAthletes(string countryName, IQueryParams queryParams = null)
+        {
+            var pipeline = new List<BsonDocument>();
+            pipeline.Add(AthleteBsonUtil.Aggregates.Match(AthleteBsonUtil.Fields.Country,countryName));
+            var result = _getAthletes(pipeline, queryParams);
+            return result;
+        }
+        public List<IAthlete> GetAthletes(int year, IQueryParams queryParams = null)
+        {
+            var pipeline = new List<BsonDocument>();
+            pipeline.Add(AthleteBsonUtil.Aggregates.Match(AthleteBsonUtil.Fields.Year,year));
+            var result = _getAthletes(pipeline, queryParams);;
+            return result;
+        }
+        public List<IAthlete> GetAthletes(int year, string countryName, IQueryParams queryParams = null)
+        {
+            var pipeline = new List<BsonDocument>();
+            pipeline.Add(AthleteBsonUtil.Aggregates.Match(AthleteBsonUtil.Fields.Year, year));
+            pipeline.Add(AthleteBsonUtil.Aggregates.Match(AthleteBsonUtil.Fields.Country, countryName));
+            var result = _getAthletes(pipeline, queryParams); ;
+            return result;
+        }
+        public List<IAthlete> GetAthletes(Models.IQueryParams queryParams = null)
+        {
+            var result = _getAthletes(null, queryParams).ToList<IAthlete>();;
+            return result;
+        }
+
+        private List<ISportMedal> _getSportGroups(IEnumerable<BsonDocument> pipeline)
+        {
+            var sportModels = default(List<ISportMedal>);
+            var groupAggregate = athletes.Aggregate(new AggregateArgs { Pipeline = pipeline });
+            sportModels = groupAggregate.Select(BsonSerializer.Deserialize<SportMedal>).ToList<ISportMedal>();
+            return sportModels;
+        }
+        public List<ISportMedal> GetSportMedals(string countryName)
+        {
+            var pipeline = new List<BsonDocument>();
+            pipeline.Add(AthleteBsonUtil.Aggregates.Match(AthleteBsonUtil.Fields.Country, countryName));
+            pipeline.Add(AthleteBsonUtil.Groups.Sport);
+            return _getSportGroups(pipeline);
+        }
+        public List<ISportMedal> GetSportMedals(int year)
+        {
+            var pipeline = new List<BsonDocument>();
+            pipeline.Add(AthleteBsonUtil.Aggregates.Match(AthleteBsonUtil.Fields.Year, year));
+            pipeline.Add(AthleteBsonUtil.Groups.Sport);
+            return _getSportGroups(pipeline);
+        }
+        public List<ISportMedal> GetSportMedals(string countryName, int year)
+        {
+            var pipeline = new List<BsonDocument>();
+            pipeline.Add(AthleteBsonUtil.Aggregates.Match(AthleteBsonUtil.Fields.Country, countryName));
+            pipeline.Add(AthleteBsonUtil.Aggregates.Match(AthleteBsonUtil.Fields.Year, year));
+            pipeline.Add(AthleteBsonUtil.Groups.Sport);
+            return _getSportGroups(pipeline);
+        }
+
+        private void _applyQueryParams(List<BsonDocument> pipeline, IQueryParams queryParams)
+        {
+            if (queryParams != null && queryParams.Sort != null)
+                pipeline.Add(
+                    AthleteBsonUtil.Aggregates.Sort
+                    (
+                        AthleteBsonUtil.CountryFieldtoBsonColumn(queryParams.SortProperty),
+                        queryParams.Sort.Value)
+                    );
+        }
+
+    }
+}
